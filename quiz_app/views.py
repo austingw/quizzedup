582d5a265578fcd6af220846e2e9fc8dc05c8c21
austingw <senortinman@gmail.com>
2024-02-05 18:31:30 -0500

    Add logic and partial template for answering questions

--- quiz_app/views.py
+++ quiz_app/views.py
@@ -1,19 +1,59 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 from django.core.paginator import Paginator
-from .models import UserScores
+from .models import Trivia, UserScores
 
 import requests
 
 
 @login_required(login_url='/login/')
 def index(request):
-    response = requests.get("https://opentdb.com/api.php?amount=1&type=boolean")
-    data = response.json()
-    question = data['results'][0]  # Extract the first question
+    if request.method == 'GET':
+        response = requests.get("https://opentdb.com/api.php?amount=1&type=boolean")
+        data = response.json()
+        if 'results' in data and len(data['results']) > 0:
+            question = data['results'][0]  # Extract the first question
+        else:
+            return HttpResponse('Error: No results from API')
+    elif request.method == 'POST':
+        question = request.POST.get('question')  # Retrieve the current question from the POST data
+        if not question:
+            return HttpResponse('Error: No current question in POST data')
+    else:
+        return HttpResponse('Invalid request')
+
     return render(request, 'index.html', {'question': question})
+
+def answer(request):
+    if request.method == 'POST':
+        question = request.POST.get('question')
+        user_answer = request.POST.get('user_answer')
+        correct_answer = request.POST.get('correct_answer')
+        print(question, user_answer, correct_answer)
+        if question is None or user_answer is None or correct_answer is None:
+            return HttpResponse('Invalid request')
+
+        correct = user_answer == correct_answer
+
+        try:
+            user_score = UserScores.objects.get(user=request.user)
+        except UserScores.DoesNotExist:
+            user_score = UserScores(user=request.user)
+
+        if correct:
+            user_score.score += 1
+
+        user_score.save()
+
+        trivia = Trivia(question=question, answer=correct_answer, correct=correct, user=request.user)
+        trivia.save()
+
+        return render(request, 'partials/answered.html', {'correct': correct, 'answer': correct_answer, 'question': question})
+
+    else:
+        return HttpResponse('Invalid request')
     
 def custom_login(request):
     if request.method == 'POST':
